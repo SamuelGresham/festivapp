569aa882bdf9ec44e06e728d26b83707e5fc9be3
Samuel Gresham <samuelgresham12@gmail.com>
2023-01-10 17:54:48 +1100

    Getting an authorisation code now

--- venv/main.py
+++ venv/main.py
@@ -1,6 +1,8 @@
-from flask import Flask, redirect
+from flask import Flask, redirect, request
 from flask import render_template 
 import urllib
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
 
 # Initialise flask app with name app. 
 # Passing name of file as the application name 
@@ -12,24 +14,25 @@ app = Flask(__name__)
 def index():
     return render_template('index.html.j2')
 
+# Redirects the user to the spotify authentication page
 @app.route("/login")
 def login():
-    client_id="a6d4ca3ba6e24f8db9a617bad5854451"
-    client_secret="3451c5e4674649bf937ab0ffba583cbd"
-    # redirect_uri="https://www.google.com"
-    redirect_uri = "http://localhost:5001"
-    scope="user-library-read"
-
+    # Define parameters for the auth request
     request_body = {
         'response_type': 'code',
-        'client_id': client_id,
-        'scope': scope, 
-        'redirect_uri': redirect_uri
+        'client_id': "a6d4ca3ba6e24f8db9a617bad5854451",
+        'scope': "user-library-read", 
+        'redirect_uri': "http://localhost:5001/auth"
     }
 
+    # Redirect the user to Spotify for authentication
     return redirect('https://accounts.spotify.com/authorize?' + urllib.parse.urlencode(request_body))
 
-@app.route("/auth")
+# The callback path for spotify auth
+@app.route("/auth", methods=["GET"])
 def auth():
-    return("yay")
+    if request.args.get("error") is not None: # if the user has not passed auth 
+        return "An error occurred"
+    else: # if the user has passed auth, redirect to get the spotify token 
+        return "The key is " + request.args["code"]
 
