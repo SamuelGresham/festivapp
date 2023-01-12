0fae166c2fc79677d853653d2a9b2e002ddf06e2
Samuel Gresham <samuelgresham12@gmail.com>
2023-01-12 17:35:46 +1100

    Started POST request for access token

--- venv/main.py
+++ venv/main.py
@@ -3,6 +3,12 @@ from flask import render_template
 import urllib
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
+import requests
+
+import util
+
+client_id = "a6d4ca3ba6e24f8db9a617bad5854451"
+client_secret = "3451c5e4674649bf937ab0ffba583cbd"
 
 # Initialise flask app with name app. 
 # Passing name of file as the application name 
@@ -26,13 +32,29 @@ def login():
     }
 
     # Redirect the user to Spotify for authentication
-    return redirect('https://accounts.spotify.com/authorize?' + urllib.parse.urlencode(request_body))
+    return redirect(util.url_get('https://accounts.spotify.com/authorize', request_body))
 
 # The callback path for spotify auth
 @app.route("/auth", methods=["GET"])
 def auth():
     if request.args.get("error") is not None: # if the user has not passed auth 
         return "An error occurred"
-    else: # if the user has passed auth, redirect to get the spotify token 
-        return "The key is " + request.args["code"]
+    else: # if the user has passed auth, redirect to get the spotify token
+        post_params = {
+            'grant_type': "authorization_code", 
+            "code": request.args.get("code"),
+            "redirect_uri": "http://localhost:5001/auth"
+        }
+        print(str(post_params["code"]))
+        post_headers = {
+            "Authorization": "Authorization: Basic " + util.convert_base64(client_id + ":" + client_secret),
+            "Content_Type": "application/x-www-form-urlencoded"
+        }
+        r = requests.post('https://api.spotify.com/v1/api/token', data=post_params, headers=post_headers)
+
+        print(r.text)
+
+        return ""
+
+
 
---
+++ venv/util.py
@@ -0,0 +1,18 @@
+# util.py
+# This file contains utility functions for completing commonly performed tasks
+
+import urllib
+import base64
+
+
+
+# Create a GET URL
+# url -> the starter API URL (e.g. https://accounts.spotify.com/authorize)
+# parameters -> object containing the parameters passed in the GET request
+def url_get (url: str, parameters: dict):
+    return str(url) + "?" + urllib.parse.urlencode(parameters)
+
+def convert_base64 (string: str): 
+    string_bytes = string.encode("ascii")
+    base64_bytes = base64.b64encode(string_bytes)
+    return base64_bytes.decode("ascii") 
