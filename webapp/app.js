7d9a4f16988af3eb86d0dbc6994d945a576e181d
Samuel Gresham <samuelgresham12@gmail.com>
2023-01-16 15:14:43 +1100

    Added routing control

--- webapp/app.js
+++ webapp/app.js
@@ -1,6 +1,3 @@
-var client_id = 'a6d4ca3ba6e24f8db9a617bad5854451';
-var redirect_uri = 'http://localhost:3000/callback';
-
 const express = require('express')
 const app = express()
 const port = 3000
@@ -11,80 +8,18 @@ const client_secret = "3451c5e4674649bf937ab0ffba583cbd"
 var querystring = require('querystring');
 var request = require('request'); // "Request" library
 
+// Get route files
+var auth = require('./routes/auth'); 
+
+
 app.get('/', function(req, res) {
   res.redirect("/login")
 })
 
-app.get('/login', function(req, res) {
-
-  var state = "abc";
-  var scope = 'user-read-private user-read-email';
-
-  console.log("Redirecting")
-  require('dotenv').config();
-  res.redirect('https://accounts.spotify.com/authorize?' +
-    querystring.stringify({
-      response_type: 'code',
-      client_id: process.env.CLIENT_ID,
-      scope: scope,
-      redirect_uri: redirect_uri,
-      state: state
-    }));
-});
+app.get('/login', auth.login);
 
 
-app.get('/callback', function(req, res) {
-
-  var code = req.query.code || null;
-  var state = req.query.state || null;
-  require('dotenv').config();
-
-  if (state === null) {
-    res.redirect('/#' +
-      querystring.stringify({
-        error: 'state_mismatch'
-      }));
-  } else {
-    var authOptions = {
-      url: 'https://accounts.spotify.com/api/token',
-      form: {
-        code: code,
-        redirect_uri: redirect_uri,
-        grant_type: 'authorization_code'
-      },
-      headers: {
-        'Authorization': 'Basic ' + (Buffer.from(process.env.CLIENT_ID + ':' + process.env.CLIENT_SECRET).toString('base64'))
-      },
-      json: true
-    };
-  }
-
-  request.post(authOptions, function(error, response, body) {
-    if (!error && response.statusCode === 200) {
-  
-      // use the access token to access the Spotify Web API
-      var token = body.access_token;
-      var options = {
-        url: 'https://api.spotify.com/v1/users/samuelgresham12',
-        headers: {
-          'Authorization': 'Bearer ' + token
-        },
-        json: true
-      };
-      request.get(options, function(error, response, body) {
-        res.render("index", {
-          title: "Welcome!",
-          message: "Welcome to Festivapp, " + body.display_name
-        })
-      });
-    } else {
-      console.error("Authentication failed")
-      res.render("Error")
-    }
-  })
-
-  
-});
+app.get('/callback', auth.callback);
 
 
 app.listen(port, () => {
---
+++ webapp/routes/auth.js
@@ -0,0 +1,68 @@
+var querystring = require('querystring');
+var request = require('request'); // "Request" library
+
+exports.login = function (req, res) {
+    var state = "abc";
+    var scope = 'user-read-private user-read-email';
+
+    console.log("Redirecting")
+    require('dotenv').config();
+    res.redirect('https://accounts.spotify.com/authorize?' +
+    querystring.stringify({
+        response_type: 'code',
+        client_id: process.env.CLIENT_ID,
+        scope: scope,
+        redirect_uri: process.env.REDIRECT_URI,
+        state: state
+    }));
+}
+
+exports.callback = function (req, res) {
+    var code = req.query.code || null;
+    var state = req.query.state || null;
+    require('dotenv').config();
+
+    if (state === null) {
+    res.redirect('/#' +
+        querystring.stringify({
+            error: 'state_mismatch'
+        }));
+    } else {
+        var authOptions = {
+            url: 'https://accounts.spotify.com/api/token',
+            form: {
+            code: code,
+            redirect_uri: process.env.REDIRECT_URI,
+            grant_type: 'authorization_code'
+        },
+        headers: {
+            'Authorization': 'Basic ' + (Buffer.from(process.env.CLIENT_ID + ':' + process.env.CLIENT_SECRET).toString('base64'))
+        },
+            json: true
+        };
+    }
+
+    request.post(authOptions, function(error, response, body) {
+        if (!error && response.statusCode === 200) {
+  
+        // use the access token to access the Spotify Web API
+        var token = body.access_token;
+        var options = {
+        url: 'https://api.spotify.com/v1/users/samuelgresham12',
+        headers: {
+            'Authorization': 'Bearer ' + token
+        },
+        json: true
+        };
+        request.get(options, function(error, response, body) {
+        res.render("index", {
+            title: "Welcome!",
+            message: "Welcome to Festivapp, " + body.display_name
+        })
+        });
+    } else {
+        console.error("Authentication failed")
+        res.render("Error")
+    }
+  })
+}
