1f4cc42e911cf598443257dedeb6a1c49df92fd9
Samuel Gresham <samuelgresham12@gmail.com>
2023-01-16 15:36:04 +1100

    Added route separation

--- webapp/app.js
+++ webapp/app.js
@@ -1,28 +1,22 @@
-const express = require('express')
-const app = express()
-const port = 3000
-app.set('view engine', 'pug')
-
-const client_secret = "3451c5e4674649bf937ab0ffba583cbd"
-
-var querystring = require('querystring');
-var request = require('request'); // "Request" library
+// Configure express.js
+const express = require('express');
+const app = express();
+const port = 3000;
+app.set('view engine', 'pug');
 
 // Get route files
 var auth = require('./routes/auth'); 
 
-
+// Index route 
 app.get('/', function(req, res) {
-  res.redirect("/login")
+   res.redirect("/login")
 })
 
 app.get('/login', auth.login);
 
-
 app.get('/callback', auth.callback);
 
-
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+   console.log(`Express application now running on http://localhost:${port}`)
 })
 
