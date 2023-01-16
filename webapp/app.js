f61296716fd3726f378ed7c7800c9232b063a06f
Samuel Gresham <samuelgresham12@gmail.com>
2023-01-16 16:06:05 +1100

    Added index page

--- webapp/app.js
+++ webapp/app.js
@@ -1,20 +1,25 @@
+const path = require('path');
+
 // Configure express.js
 const express = require('express');
 const app = express();
 const port = 3000;
 app.set('view engine', 'pug');
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Get route files
 var auth = require('./routes/auth'); 
 
 // Index route 
 app.get('/', function(req, res) {
-   res.redirect("/login")
+   res.render("index")
 })
 
 app.get('/login', auth.login);
 
 app.get('/callback', auth.callback);
+
+app.get('/home', )
 
 app.listen(port, () => {
    console.log(`Express application now running on http://localhost:${port}`)
